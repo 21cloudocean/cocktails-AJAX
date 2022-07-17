f44ba0d526c130046730e2d6b5a6e3bf0e65e1c3
21cloudocean <437508630@qq.com>
2022-07-18 00:13:26 +0800

    displaySingleDrink

--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -2,7 +2,6 @@ import fetchDrinks from "./fetchDrinks.js";
 import displayDrinks from "./displayDrinks.js";
 import setDrink from "./setDrink.js";
 const showDrinks = async (url) => {
-  console.log(url);
   // fetch drinks
   const data = await fetchDrinks(url);
   // display drinks
